880b51cfe51a95318f1532a3e185f1548f386682
Firuddin <Firuddinaliyev888@gmail.com>
2024-01-04 08:17:49 -0800

    add High-quality

--- src/Pages/HomePage/Home.jsx
+++ src/Pages/HomePage/Home.jsx
@@ -1,19 +1,30 @@
 
 import { Navigation } from '../../Components/Navigation/Navigation'
-
 const Home = () => {
   return (
     <>
       <Navigation />
       <div>
-        <div className='flex flex-col items-center mt-20 mr-58 gap-5 h-screen text-center'>
+        <div className='flex flex-col items-center mt-20 mr-58 gap-7  text-center'>
           <h1 className="text-5xl font-bold text-pirmary ">The Best Software to Grow <br />
            your Sales and Services</h1>
           <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr,
              sed diam nonumy eirmod tempor <br /> invidunt ut labore et dolore magna aliquyam erat.</p>
-             <input type="text" placeholder='Enter your email' />
+             <div className='flex items-center justify-center'>
+             <input type="text" placeholder='Enter your email' className='w-80 px-4 py-2 border rounded-md' />
+             <button className='relative right- px-4 py-2 bg-pirmary text-white rounded-md'>Get Free trial</button>
+             </div>
         </div>
+        <div className='flex items-center justify-center h-50 w-1/1 mt-20'>
+        <img src='../../../public/weet.png' alt className='rounded-lg shadow-lg w-2/3'/>
       </div>
+      </div>
+      <div className='flex flex-col items-center mt-20 mr-58 gap-4  text-center'>
+        <p className='text-pink  font-bold'>High-quality</p>
+        <h2 className='text-4xl text-center font-bold text-pirmary '>We have the Best Solution <br />
+         for your Business</h2>
+      </div>
+      
     </>
   )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -8,9 +8,9 @@ export default {
     extend: {
       colors:{
         'pirmary': "#1D2130",
-        'secondary': "#9A7AF1",
+        'secondary': "#E0E4FC",
         'tartiary': "#707070",
-        'pink':"#EE9AE5"
+        'pink':"#373FFF"
               }
     },
   },
