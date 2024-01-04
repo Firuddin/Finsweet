6f77ec371d6cc8633a4d9c91b870965c43d08ae1
Firuddin <Firuddinaliyev888@gmail.com>
2024-01-04 00:32:05 -0800

    the best add

--- src/Pages/HomePage/Home.jsx
+++ src/Pages/HomePage/Home.jsx
@@ -1,9 +1,20 @@
-import React from 'react'
+
 import { Navigation } from '../../Components/Navigation/Navigation'
 
 const Home = () => {
   return (
-<Navigation/>
+    <>
+      <Navigation />
+      <div>
+        <div className='flex flex-col items-center mt-20 mr-58 gap-5 h-screen text-center'>
+          <h1 className="text-5xl font-bold text-pirmary ">The Best Software to Grow <br />
+           your Sales and Services</h1>
+          <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr,
+             sed diam nonumy eirmod tempor <br /> invidunt ut labore et dolore magna aliquyam erat.</p>
+             <input type="text" placeholder='Enter your email' />
+        </div>
+      </div>
+    </>
   )
 }
 
