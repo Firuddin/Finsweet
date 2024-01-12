beed7847485e7be31fea709f05f226dc533b9455
Firuddin <Firuddinaliyev888@gmail.com>
2024-01-12 13:31:35 -0800

    add time tracker

--- src/Pages/Product/Product.jsx
+++ src/Pages/Product/Product.jsx
@@ -20,39 +20,61 @@ const Product = () => {
         </div>
       </div>
       <LogoIpsum />
-      <div className="bg-six">
+      <div className="bg-six pb-10">
         <h2 className="text-center font-bold text-3xl mt-20 pt-20 pb-10">Get the best out of your company <br /> with our service</h2>
-        <div className="bg-four w-160 ml-20 mr-20 rounded-3xl">
-          <div className="flex items-center justify-center gap-10">
+        <div className="bg-four w-160 ml-20 mr-20  rounded-3xl">
+          <div className="flex items-center justify-center gap-10 p-10">
             <div>
               <img src="../../../public/Product/Icon (7).png" alt="" />
-              <h5 className="font-bold text-pirmary">Security and privacy</h5>
+              <h5 className="font-bold text-pirmary pt-2 pb-2">Security and privacy</h5>
               <p>Lorem ipsum dolor sit amet, consetetur sadipscing <br /> elitr, sed diam nonumy eirmod tempor.</p>
             </div>
             <div><img src="../../../public/Product/Icon (5).png" alt="" />
-              <h5 className="font-bold text-pirmary">Security and privacy</h5>
+              <h5 className="font-bold text-pirmary pt-2 pb-2">Built-in AI features
+              </h5>
               <p>Lorem ipsum dolor sit amet, consetetur sadipscing <br /> elitr, sed diam nonumy eirmod tempor.</p></div>
             <div><img src="../../../public/Product/Icon (6).png" alt="" />
-              <h5 className="font-bold text-pirmary">Security and privacy</h5>
+              <h5 className="font-bold text-pirmary pt-2 pb-2">Collaborate with others</h5>
               <p>Lorem ipsum dolor sit amet, consetetur sadipscing <br /> elitr, sed diam nonumy eirmod tempor.</p></div>
           </div>
-          <div className="flex items-center justify-center gap-10">
+          <div className="flex items-center justify-center gap-10 p-10">
             <div>
-            <img src="../../../public/Product/Icon (2).png" alt="" />
-              <h5 className="font-bold text-pirmary">Security and privacy</h5>
+              <img src="../../../public/Product/Icon (2).png" alt="" />
+              <h5 className="font-bold text-pirmary pt-2 pb-2">Real time sync
+              </h5>
               <p>Lorem ipsum dolor sit amet, consetetur sadipscing <br /> elitr, sed diam nonumy eirmod tempor.</p>
             </div>
             <div>
-            <img src="../../../public/Product/Icon (3).png" alt="" />
-              <h5 className="font-bold text-pirmary">Security and privacy</h5>
+              <img src="../../../public/Product/Icon (3).png" alt="" />
+              <h5 className="font-bold text-pirmary pt-2 pb-2">Built-in AI features</h5>
               <p>Lorem ipsum dolor sit amet, consetetur sadipscing <br /> elitr, sed diam nonumy eirmod tempor.</p>
             </div>
             <div>
-            <img src="../../../public/Product/Icon (4).png" alt="" />
-              <h5 className="font-bold text-pirmary">Security and privacy</h5>
+              <img src="../../../public/Product/Icon (4).png" alt="" />
+              <h5 className="font-bold text-pirmary pt-2 pb-2">Easy notes organization</h5>
               <p>Lorem ipsum dolor sit amet, consetetur sadipscing <br /> elitr, sed diam nonumy eirmod tempor.</p>
             </div>
           </div>
+        </div>
+      </div>
+      <div className="flex items-center ml-20 mr-0 gap-60 mt-20">
+        <div>
+          <h3 className="text-two font-bold text-1xl pb-5">Marketing insights</h3>
+          <h2 className="font-bold text-pirmary text-4xl pb-8">Data-driven client <br /> feedback</h2>
+          <p className="pb-10">Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy <br /> eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam <br /> voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita <br /> kasd gubergren.</p>
+        </div>
+        <div>
+          <img className="max-w-120 max-h-80 mr-20" src="../../../public/Product/Image (2).png" alt="" />
+        </div>
+      </div>
+
+      <div className="flex items-center ml-20 mr-0 gap-60 mt-20">
+        <div>
+          <img className="max-w-120 max-h-80 mr-20" src="../../../public/Product/Image (3).png" alt="" /></div>
+        <div>
+          <h3 className="text-two font-bold text-1xl pb-5">Time tracker</h3>
+          <h2 className="font-bold text-pirmary text-4xl pb-8">Track your project <br /> performance</h2>
+          <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy <br /> eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam <br /> voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita <br /> kasd gubergren.</p>
         </div>
       </div>
     </>
