a72d91a79fc93bfb8a845fa34af19faea68e08a3
Firuddin <Firuddinaliyev888@gmail.com>
2024-01-14 11:35:43 -0800

    The end

--- src/Pages/Blog/Blog.jsx
+++ src/Pages/Blog/Blog.jsx
@@ -1,20 +1,37 @@
+import Footer from "../../Components/Footer/Footer"
 import { Navigation } from "../../Components/Navigation/Navigation"
 
 
 const Blog = () => {
   return (
-   <>
-   <Navigation/>
-   <div>
-    <div>
-      <h3>Trending Post</h3>
-      <h3>New invoicing features to help you get paid faster</h3>
-      <p>Over the past few months, we’ve added several new features to SaaS <br /> Invoicing to help any business get paid faster and streamline their collection <br /> workflows.</p>
-      <p>Luke Matthews l  November 8, 2021</p>
-    </div>
-    <div></div>
-   </div>
-   </>
+    <>
+      <Navigation />
+      <div className="flex flex-col items-center ">
+        <h4 className="font-bold text-pirmary text-5xl mt-10 mb-5">Read our latest blogs</h4>
+        <p className="text-center mb-10">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in eros <br />
+          elementum tristique. Duis cursus, mi quis viverra ornare, eros dolor.</p>
+      </div>
+
+      <div className="flex items-center justify-center border border-solid rounded-3xl pt-10 pb-10 gap-60 ml-20 mr-20">
+        <div>
+          <h3 className="text-pink font-bold text-4xl mb-4">Trending Post</h3>
+          <h3 className="font-bold text-pirmary text-3xl mb-4">New invoicing features to <br /> help you get paid faster</h3>
+          <p className="mb-7">Over the past few months, we’ve added several new features to SaaS <br /> Invoicing to help any business get paid faster and streamline their collection <br /> workflows.</p>
+          <p>Luke Matthews l  November 8, 2021</p>
+        </div>
+        <div>
+          <img className="w-80 rounded-3xl" src="../../../public/blog/pexels-photo-3184357.webp" alt="" />
+        </div>
+      </div>
+
+      <div className="mt-20 mb-10">
+        <h3 className="flex items-center justify-center text-pirmary font-bold text-5xl">All posts</h3>
+      </div>
+      <div className="flex items-center justify-center mb-20">
+        <img src="../../../public/blog/List (1).png" alt="" />
+      </div>
+      <Footer />
+    </>
   )
 }
 
